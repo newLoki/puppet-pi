def0fff33ae0f7f3390da00a6773157f1d3e79f8
newLoki <newloki91@googlemail.com>
2012-11-29 18:33:07 +0100

    fix the check if ruby-dev is installed

--- install.sh
+++ install.sh
@@ -6,10 +6,13 @@ git pull
 if [ ! -e /usr/bin/librarian-puppet ] 
 then
   #ruby-dev package is needed to build json-gem extension, which is required by librarian-puppet
-  if [ ! `dpkg -l | grep -i 'ruby-dev'` ]
-  then
-    sudo apt-get install ruby-dev
+  PKG_OK=$(dpkg-query -W --showformat='${Status}\n' ruby-dev|grep "install ok installed")
+  echo Checking for somelib: $PKG_OK
+  if [ "" == "$PKG_OK" ]; then
+    sudo apt-get --force-yes --yes install ruby-dev
   fi
+  
+  
   #now install librarian-puppet
   sudo gem install librarian-puppet  
 fi
