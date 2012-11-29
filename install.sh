02643c9ca179037bffba857abae23a54fb51ba3c
newLoki <newloki91@googlemail.com>
2012-11-29 18:07:12 +0100

    add some flavour to install librarian-puppet and run it before puppet to get all dependencies

--- install.sh
+++ install.sh
@@ -1,3 +1,12 @@
 #get newest source
 git pull
+
+#check if puppet-librarian is availeable
+if [ ! -e /usr/bin/librarian-puppet ] then
+  sudo gem install librarian-puppet  
+fi
+#install dependencies
+librarian-puppet install
+
+#run puppet
 puppet apply --modulepath=./modules -l /tmp/puppet.log ./manifests/pi.pp
