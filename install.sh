03edcb53c79967218d1438ef06b8c0b9c97683af
newLoki <newloki91@googlemail.com>
2012-11-29 18:26:38 +0100

    add installation of ruby-dev dependency to installation process of librarian-puppet

--- install.sh
+++ install.sh
@@ -5,6 +5,12 @@ git pull
 #check if puppet-librarian is availeable
 if [ ! -e /usr/bin/librarian-puppet ] 
 then
+  #ruby-dev package is needed to build json-gem extension, which is required by librarian-puppet
+  if [ ! `dpkg -l | grep -i 'ruby-dev'` ]
+  then
+    sudo apt-get install ruby-dev
+  fi
+  #now install librarian-puppet
   sudo gem install librarian-puppet  
 fi
 #install dependencies
